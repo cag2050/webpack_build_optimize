538a6ad5dfc3ea6590c692efd040ef6d65b576d9
cag2050 <cag2050@163.com>
2017-08-07 19:12:50 +0800

    m

--- config/index.js
+++ config/index.js
@@ -3,11 +3,11 @@ var path = require('path')
 
 module.exports = {
   build: {
-    env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    env: require('./prod.env'), // 使用 config/prod.env.js 中定义的编译环境
+    index: path.resolve(__dirname, '../dist/index.html'), // 编译输出的 index.html 文件
+    assetsRoot: path.resolve(__dirname, '../dist'), // 编译输出的静态资源路径
+    assetsSubDirectory: 'static', // 编译输出的二级目录
+    assetsPublicPath: '/', // 编译发布的根目录，可配置为资源服务器域名或 CDN 域名
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
@@ -22,11 +22,11 @@ module.exports = {
     bundleAnalyzerReport: process.env.npm_config_report
   },
   dev: {
-    env: require('./dev.env'),
-    port: 8080,
+    env: require('./dev.env'), // 使用 config/dev.env.js 中定义的编译环境
+    port: 8080, // 运行测试页面的端口
     autoOpenBrowser: true,
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsSubDirectory: 'static',  // 编译输出的二级目录
+    assetsPublicPath: '/', // 编译发布上线路径的根目录，可配置为资源服务器域名或 CDN 域名
     proxyTable: {},
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
